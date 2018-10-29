533530bdbe2eaca4353c3d8bba22b0cabe977355
tdocal <35549686+tdocal@users.noreply.github.com>
2018-10-29 11:55:36 -0600

    0.0.2 Removed unnecessary layers
    Changed how bridge rating was determined to display points. Should be more accurate. May change points in the future. Removed Denver border.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-var view;
-
 require([
     "esri/Map",
     "esri/views/MapView",
@@ -25,7 +23,7 @@ require([
         basemap: "streets-relief-vector"
     });
     
-    view = new MapView({
+    var view = new MapView({
         map: map,
         container: "viewDiv",
         constraints: {
@@ -74,85 +72,48 @@ require([
 
     view.ui.add(basemapExpand, "top-left");
     
-    //Remove later
-    var denverBorder = {
-        type: "simple-fill",
-        color: [99, 99, 99, 0],
-        outline: {
-            color: [99, 99, 99, 1],
-            width: 2
-        }
-    };
-    
-    //Remove later
-    var denverRenderer = {
-        type: "simple",
-        symbol: denverBorder
-    };
-    
-    var goodMarker = {
-        type: "picture-marker",
-        url: "https://static.arcgis.com/images/Symbols/Basic/GreenSphere.png",
-        width: "30px",
-        height: "30px",
-        angle: 30
-    }
-    
-    var fairMarker = {
-        type: "picture-marker",
-        url: "https://static.arcgis.com/images/Symbols/Basic/YellowSphere.png",
-        width: "30px",
-        height: "30px",
-        angle: 30
-    }
-    
-    var poorMarker = {
-        type: "picture-marker",
-        url: "https://static.arcgis.com/images/Symbols/Basic/OrangeSphere.png",
-        width: "30px",
-        height: "30px",
-        angle: 30
-    }
-    
-    var failMarker = {
-        type: "picture-marker",
-        url: "https://static.arcgis.com/images/Symbols/Basic/RedSphere.png",
-        width: "30px",
-        height: "30px",
-        angle: 30
-    }
-    
-    var pointRendererGood = {
+    var pointRenderer = {
         type: "unique-value",
-        defaultSymbol: goodMarker,
-        field: "SUFFICIE_1",
+        valueExpressionTitle: " ",
+        defaultSymbol: {
+            type: "picture-marker",
+            url: "https://static.arcgis.com/images/Symbols/Basic/PurpleSphere.png",
+            width: "30px",
+            height: "30px"
+        },
+        defaultLabel: "Other",
+        field: "CAT10",
         uniqueValueInfos: [{
-            value: 80,
-            symbol: goodMarker
+            value: "G",
+            symbol: {
+                type: "picture-marker",
+                url: "https://static.arcgis.com/images/Symbols/Basic/GreenSphere.png",
+                width: "30px",
+                height: "30px"
+            },
+            label: "Good"
+        }, {
+            value: "F",
+            symbol: {
+                type: "picture-marker",
+                url: "https://static.arcgis.com/images/Symbols/Basic/YellowSphere.png",
+                width: "30px",
+                height: "30px"
+            },
+            label: "Fair"
+        }, {
+            value: "P",
+            symbol: {
+                type: "picture-marker",
+                url: "https://static.arcgis.com/images/Symbols/Basic/OrangeSphere.png",
+                width: "30px",
+                height: "30px"
+            },
+            label: "Poor"
         }]
     };
     
-    var pointRendererFair = {
-        type: "unique-value",
-        defaultSymbol: fairMarker,
-        field: "SUFFICIE_1",
-        uniqueValueInfos: [{
-            value: 20,
-            symbol: failMarker
-        }]
-    };
-    
-    var pointRendererPoor = {
-        type: "unique-value",
-        defaultSymbol: poorMarker,
-        field: "SUFFICIE_1",
-        uniqueValueInfos: [{
-            value: 20,
-            symbol: failMarker
-        }]
-    };
-    
-    var goodSym = {
+    var bridgeRenderer = {
         type: "simple",
         symbol: {
             type: "simple-fill",
@@ -168,20 +129,16 @@ require([
             field: "SUFFICIE_1",
             stops: [{
                 value: 0,
-                color: [255, 0, 0, 0.85],
-                label: "Fail: Sufficiency < 10"
+                color: [158, 154, 200, 0.85]
             }, {
                 value: 10,
-                color: [255, 127, 0, 0.85],
-                label: "Poor: Sufficiency < 50"
+                color: [255, 127, 0, 0.85]
             }, {
                 value: 50,
-                color: [255, 255, 51, 0.85],
-                label: "Fair: Sufficiency > 50"
+                color: [255, 255, 51, 0.85]
             }, {
-                value: 100,
-                color: [0, 255, 0, 0.85],
-                label: "Good: Sufficency > 80"
+                value: 80,
+                color: [0, 255, 0, 0.85]
             }]
         }]
     };
@@ -189,48 +146,26 @@ require([
     var bridgeLayer = new FeatureLayer({
         url: "https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Denver_Bridge_Integrity_Ref/FeatureServer/1",
         definitionExpression: "DECK_COND_ <> 'N' AND SUPERSTRUC <> 'N' AND SUBSTRUCTU <> 'N'",
-        renderer: goodSym,
+        renderer: bridgeRenderer,
+        legendEnabled: false
+    });
+    map.add(bridgeLayer);
+    
+    var pointsLayer = new FeatureLayer({
+        url: "https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Denver_Bridge_Integrity_Ref/FeatureServer/0",
+        definitionExpression: "DECK_COND_ <> 'N' AND SUPERSTRUC <> 'N' AND SUBSTRUCTU <> 'N'",
+        renderer: pointRenderer,
         popupTemplate: {
             title: "{BRIDGE_NAM}",
             content: "Structure ID: {STRUCTURE_NUM}<br>Built in: {YEARBUILT}<br>Bridge Sufficieny Rating: <strong>{SUFFICIE_1}</strong><br>Deck Condition Rating: {DECK_COND_}<br>Superstructure Rating: {SUPERSTRUC}<br>Substructure Rating: {SUBSTRUCTU}<br>Culvert Condition: {CULVERT_CO}<br>Last Inspected: {YEAR_ADT_0}<br>Next Inspection: {YEAR_OF_FU}"
         }
     });
-    map.add(bridgeLayer);
-    
-    var goodPointsLayer = new FeatureLayer({
-        url: "https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Denver_Bridge_Integrity_Ref/FeatureServer/0",
-        definitionExpression: "DECK_COND_ <> 'N' AND SUPERSTRUC <> 'N' AND SUBSTRUCTU <> 'N' AND SUFFICIE_1 >= '80'",
-        renderer: pointRendererGood,
-        legendEnabled: false
-    });
-    map.add(goodPointsLayer);
-    
-    var fairPointsLayer = new FeatureLayer({
-        url: "https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Denver_Bridge_Integrity_Ref/FeatureServer/0",
-        definitionExpression: "DECK_COND_ <> 'N' AND SUPERSTRUC <> 'N' AND SUBSTRUCTU <> 'N' AND SUFFICIE_1 >= '50' AND SUFFICIE_1 < '80'",
-        renderer: pointRendererFair,
-        legendEnabled: false
-    });
-    map.add(fairPointsLayer);
-    
-    var poorPointsLayer = new FeatureLayer({
-        url: "https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Denver_Bridge_Integrity_Ref/FeatureServer/0",
-        definitionExpression: "DECK_COND_ <> 'N' AND SUPERSTRUC <> 'N' AND SUBSTRUCTU <> 'N' AND SUFFICIE_1 >= '10' AND SUFFICIE_1 < '50'",
-        renderer: pointRendererPoor,
-        legendEnabled: false
-    });
-    map.add(poorPointsLayer);
-    
-    var denverBoundary = new FeatureLayer({
-        url: "https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Denver_Bridge_Integrity_Ref/FeatureServer/2",
-        renderer: denverRenderer
-    });
-    map.add(denverBoundary);
+    map.add(pointsLayer);
         
     var legend = new Legend({
         view: view,
         layerInfos: [{
-            layer: bridgeLayer,
+            layer: pointsLayer,
             title: "Denver Bridge Integrity"
         }]
     });
